ae7609780b114a663cc2f6df528cfd44dfbdc070
Florian Kaiser <kaiser@punkt.de>
2017-01-25 16:55:27 +0100

    Remove filemetadata dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,8 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 1,
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.0.99',
-            'filemetadata' => '',
+            'typo3' => '7.6.0-8.0.99'
         ),
         'conflicts' => array(),
         'suggests' => array(
